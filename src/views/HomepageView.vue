9c163c408c7eae7ddf6aff4f9355f9e482ba6826
hiphop200199 <hiphop200199@gmail.com>
2024-10-03 23:18:29 +0800

    keep adjust homepage

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -1,10 +1,54 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+const cardData = [
+  { title: 'lorem', price: '$300' },
+  { title: 'yayaya', price: '$400' },
+  { title: 'vbn', price: '$123' },
+  { title: 'lore', price: '$300' },
+  { title: 'yayay', price: '$400' },
+  { title: 'vb', price: '$123' },
+  { title: 'loremj', price: '$300' },
+  { title: 'yayayaj', price: '$400' }
+]
+let slideDistance = ref(0)
+let scrollY = ref(0)
+const cards = ref([])
+let slide
+onMounted(() => {
+  window.addEventListener('scroll', () => (scrollY.value = window.scrollY))
+  slide = (dir) => {
+    dir === 'left' ? (slideDistance.value -= 250) : (slideDistance.value += 250)
+    cards.value.style.transform = `translateX(${slideDistance.value}px)`
+  }
+})
+</script>
 
 <template>
   <div id="homepage-container">
     <img id="img-auto-slide" src="../assets/man-1690965_640.jpg" alt="" />
     <h1>熱門商品</h1>
-    <div id="new-items-slider"></div>
+    <div id="new-items-slider">
+      <button
+        id="slide-left"
+        @click="slide('left')"
+        v-if="scrollY === 800 && slideDistance > -2000"
+      >
+        <img src="../assets/left-arrow.png" alt="" />
+      </button>
+      <div id="cards" ref="cards">
+        <div class="product-card" v-for="(card, index) in cardData" :key="index">
+          <router-link to="detail">
+            <img class="product-cover" src="../assets/castle.png" alt="" />
+          </router-link>
+          <p class="product-title">{{ card.title }}</p>
+          <p class="product-price">{{ card.price }}</p>
+        </div>
+      </div>
+
+      <button id="slide-right" @click="slide('right')" v-if="scrollY === 800 && slideDistance < 0">
+        <img src="../assets/right-arrow.png" alt="" />
+      </button>
+    </div>
     <h2>讀者回饋</h2>
     <section id="feedbacks">
       <div class="feedback-card">
@@ -76,8 +120,10 @@
     <section id="to-search">
       <h3>搜尋</h3>
       <p>尋找最喜歡的那一本書</p>
-      <img src="../assets/blonde-1866951_1280.jpg" alt="" />
-      <router-link to="/list">連結</router-link>
+      <div id="pic-and-link">
+        <img src="../assets/blonde-1866951_1280.jpg" alt="" />
+        <router-link to="/list">連結</router-link>
+      </div>
     </section>
   </div>
 </template>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -2,12 +2,93 @@
 
 <template>
   <main>
+    <h1>本月新書</h1>
+    <div id="product-container">
+      <div class="product-card">
+        <router-link to="detail">
+          <img class="product-cover" src="../assets/castle.png" alt="" />
+        </router-link>
+        <p class="product-title">Lorem, ipsum.</p>
+        <p class="product-price">$300</p>
+      </div>
+      <div class="product-card">
+        <router-link to="detail">
+          <img class="product-cover" src="../assets/castle.png" alt="" />
+        </router-link>
+        <p class="product-title">Lorem, ipsum.</p>
+        <p class="product-price">$300</p>
+      </div>
+      <div class="product-card">
+        <router-link to="detail">
+          <img class="product-cover" src="../assets/castle.png" alt="" />
+        </router-link>
+        <p class="product-title">Lorem, ipsum.</p>
+        <p class="product-price">$300</p>
+      </div>
+      <div class="product-card">
+        <router-link to="detail">
+          <img class="product-cover" src="../assets/castle.png" alt="" />
+        </router-link>
+        <p class="product-title">Lorem, ipsum.</p>
+        <p class="product-price">$300</p>
+      </div>
+      <div class="product-card">
+        <router-link to="detail">
+          <img class="product-cover" src="../assets/castle.png" alt="" />
+        </router-link>
+        <p class="product-title">Lorem, ipsum.</p>
+        <p class="product-price">$300</p>
+      </div>
+      <div class="product-card">
+        <router-link to="detail">
+          <img class="product-cover" src="../assets/castle.png" alt="" />
+        </router-link>
+        <p class="product-title">Lorem, ipsum.</p>
+        <p class="product-price">$300</p>
+      </div>
+      <div class="product-card">
+        <router-link to="detail">
+          <img class="product-cover" src="../assets/castle.png" alt="" />
+        </router-link>
+        <p class="product-title">Lorem, ipsum.</p>
+        <p class="product-price">$300</p>
+      </div>
+      <div class="product-card">
+        <router-link to="detail">
+          <img class="product-cover" src="../assets/castle.png" alt="" />
+        </router-link>
+        <p class="product-title">Lorem, ipsum.</p>
+        <p class="product-price">$300</p>
+      </div>
+      <div class="product-card">
+        <router-link to="detail">
+          <img class="product-cover" src="../assets/castle.png" alt="" />
+        </router-link>
+        <p class="product-title">Lorem, ipsum.</p>
+        <p class="product-price">$300</p>
+      </div>
+      <div class="product-card">
+        <router-link to="detail">
+          <img class="product-cover" src="../assets/castle.png" alt="" />
+        </router-link>
+        <p class="product-title">Lorem, ipsum.</p>
+        <p class="product-price">$300</p>
+      </div>
+      <div class="product-card">
+        <router-link to="detail">
+          <img class="product-cover" src="../assets/castle.png" alt="" />
+        </router-link>
+        <p class="product-title">Lorem, ipsum.</p>
+        <p class="product-price">$300</p>
+      </div>
+    </div>
+    <h1>特定書本查尋</h1>
     <section id="class-filters">
       <button>文學</button>
       <button>科技</button>
       <button>語言</button>
     </section>
-    <input type="search" name="" id="ambiguous-search" placeholder="請輸入書名..." />
+    <input type="search" name="" id="ambiguous-search" placeholder="請輸入關鍵字..." />
     <div id="product-container">
       <div class="product-card">
         <router-link to="detail">
