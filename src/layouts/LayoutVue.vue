c9dee8bbc4b397a3182f1d244a8dfbf9296df7d2
hiphop200199 <hiphop200199@gmail.com>
2025-02-09 12:12:47 +0800

    s

--- src/layouts/LayoutVue.vue
+++ src/layouts/LayoutVue.vue
@@ -58,9 +58,7 @@ console.log(authStore.isLogin)
     </header>
     <router-view />
     <footer>
-      <a href="https://hiphop200199.github.io/eric_bookstore/"
-        ><img src="../assets/book.png" alt="logo"
-      /></a>
+      <a href="https://eric-bookstore.vercel.app/"><img src="../assets/book.png" alt="logo" /></a>
       <p id="copyright">Copyright © Reading 2024 | All Rights Reserved.</p>
       <a href="https://www.instagram.com/" target="_blank"
         ><img src="../assets/instagram.png" alt="" id="instagram"
